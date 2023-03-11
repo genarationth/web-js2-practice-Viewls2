f54af01d0e2438baa2d91d75974319d1a9f05f06
Viewls2 <mahasamut.kh@gmail.com>
2023-03-11 17:02:11 +0700

    feat(3): finish part2.challeng2 & add file for part2 challenge1

---
+++ p2ch2.js
@@ -0,0 +1,11 @@
+// Part 2 Challenge 2
+
+let user = {
+    firstName: 'Mahasamut',
+    homeTown: 'Bangkok',
+    hobby: 'listen to music',
+
+}
+
+console.log(`Hi, my name is ${user.firstName}.
+ I live in ${user.homeTown} and enjoy ${user.hobby} while I've freetime.`)
