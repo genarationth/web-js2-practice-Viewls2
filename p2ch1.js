92cf10ffeb58a9b1d5fab1db6d76ec863cc04c13
Viewls2 <mahasamut.kh@gmail.com>
2023-03-11 17:13:57 +0700

    feat(4): finish part2 challenge1

---
+++ p2ch1.js
@@ -0,0 +1,5 @@
+let birthYear = 1999;
+let futureYear = 2023;
+currentAge = futureYear - birthYear;
+possibleAge = currentAge - 1;
+console.log(`If you were born in ${birthYear}, then in ${futureYear} you'll be ${possibleAge} or ${currentAge} (depending on the month in ${futureYear}).`);
